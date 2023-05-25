cffc3bfe39b3a1184ab8125fced41dba291bafce
Tales Xavier <talesxavier1@hotmail.com>
2023-05-24 21:00:31 -0300

    Melhoria Criação dos métodos para buscar position por query.

--- Repository/Interfaces/IPositionRepository.cs
+++ Repository/Interfaces/IPositionRepository.cs
@@ -5,10 +5,13 @@ public interface IPositionRepository {
 
     public Boolean addPosition(PositionModel positionsModel, UserModel user);
     public List<PositionModel> getPositions(int skip, int take);
+    public List<PositionModel> getPositionsByQuery(int skip, int take, String query);
     public PositionModel getPositionById(string codigo);
     public Boolean updatePosition(PositionModel positionsModel, UserModel User);
     public Boolean deletePosition(String codigo);
     public Boolean logicalDeletePosition(String[] codigos, UserModel user);
     public long count();
+    public long count(String query);
+
 }
 
