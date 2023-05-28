0c124a886a8c44d86133437c03a9e380763d315f
Tales Xavier <talesxavier1@hotmail.com>
2023-05-28 19:00:45 -0300

    Melhoria. Criação da API de Teacher.

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SingularChatAPIs.Models;
+using SingularChatAPIs.Repository.Implementations;
+using SingularChatAPIs.ResponseModels;
+using SingularChatAPIs.ResponseModels.ENUMs;
+
+namespace SingularChatAPIs.Controllers;
+
+[ApiController]
+[Route("Teacher")]
+public class TeacherController : Controller {
+
+    [HttpPost]
+    [Route("logicalDeleteTeacher")]
+    public ActionResult<OperationResponseModel> logicalDeleteTeacher([FromHeader] String userToken, [FromQuery] string codigo) {
+
+        OperationResponseModel response = new();
+        UserRepository userRepository = new();
+
+        if (!new UserRepository().validateToken(userToken)) {
+            response.oparationStatus = Status.NOK;
+            response.message = "userToken Inválido.";
+            return StatusCode(401, response);
+        }
+        UserModel user = userRepository.getUserByToken(userToken);
+
+        Boolean operationResult = new TeacherRepository().logicalDeleteTeacher(codigo, user);
+
+        response.oparationStatus = operationResult ? Status.OK : Status.NOK;
+
+        return StatusCode(200, response);
+    }
+
+    [HttpPost]
+    [Route("addTeacher")]
+    public ActionResult<OperationResponseModel> addTeacher([FromHeader] String userToken, [FromBody] TeacherModel teacher) {
+        OperationResponseModel response = new();
+        UserRepository userRepository = new();
+
+        if (!userRepository.validateToken(userToken)) {
+            response.oparationStatus = Status.NOK;
+            response.message = "userToken Inválido.";
+            return StatusCode(401, response);
+        }
+
+        UserModel user = userRepository.getUserByToken(userToken);
+
+        Boolean operationResult = new TeacherRepository().addTeacher(teacher, user);
+
+        response.oparationStatus = operationResult ? Status.OK : Status.NOK;
+        response.message = operationResult ? "Registro Criado." : "Não foi possível criar registro.";
+        response.data = teacher;
+
+        return StatusCode(200, response);
+    }
+
+    [HttpPost]
+    [Route("modifyTeacher")]
+    public ActionResult<OperationResponseModel> modifyTeacher([FromHeader] String userToken, [FromBody] TeacherModel teacher) {
+        OperationResponseModel response = new();
+        UserRepository userRepository = new();
+
+        if (!userRepository.validateToken(userToken)) {
+            response.oparationStatus = Status.NOK;
+            response.message = "userToken Inválido.";
+            return StatusCode(401, response);
+        }
+        UserModel user = userRepository.getUserByToken(userToken);
+
+        new TeacherRepository().addTeacher(teacher, user);
+        return StatusCode(200, response);
+    }
+
+    [HttpGet]
+    [Route("getTeacherList")]
+    public ActionResult<OperationResponseModel> getTeacherList([FromHeader] String userToken, [FromQuery] int skip, int take) {
+        OperationResponseModel response = new();
+
+        if (!new UserRepository().validateToken(userToken)) {
+            response.oparationStatus = Status.NOK;
+            response.message = "userToken Inválido.";
+            return StatusCode(401, response);
+        }
+
+        List<TeacherModel> result = new TeacherRepository().getTeacherList(skip, take);
+        response.oparationStatus = Status.OK;
+        response.message = "";
+        response.data = result;
+
+        return StatusCode(200, response);
+    }
+
+    [HttpPost]
+    [Route("getTeacherByQuery")]
+    public ActionResult<OperationResponseModel> getTeacherByQuery([FromHeader] String userToken, [FromBody] string queryB64, [FromQuery] int skip, [FromQuery] int take) {
+        OperationResponseModel response = new();
+
+        if (!new UserRepository().validateToken(userToken)) {
+            response.oparationStatus = Status.NOK;
+            response.message = "userToken Inválido.";
+            return StatusCode(401, response);
+        }
+
+        try {
+            byte[] valueBytes = System.Convert.FromBase64String(queryB64);
+            string stringFilter = Uri.UnescapeDataString(System.Text.Encoding.UTF8.GetString(valueBytes));
+
+            response.data = new TeacherRepository().getTeacherByStringQuery(stringFilter, skip, take);
+            response.oparationStatus = Status.OK;
+
+            return StatusCode(200, response);
+        } catch (Exception e) {
+            response.oparationStatus = Status.NOK;
+            response.message = e.ToString();
+            return StatusCode(500, response);
+        }
+    }
+
+    [HttpPost]
+    [Route("countTeachersByQuery")]
+    public ActionResult<OperationResponseModel> countTeachersByQuery([FromHeader] String userToken, [FromBody] string queryB64) {
+        OperationResponseModel response = new();
+
+        if (!new UserRepository().validateToken(userToken)) {
+            response.oparationStatus = Status.NOK;
+            response.message = "userToken Inválido.";
+            return StatusCode(401, response);
+        }
+
+        try {
+            byte[] valueBytes = System.Convert.FromBase64String(queryB64);
+            string stringFilter = Uri.UnescapeDataString(System.Text.Encoding.UTF8.GetString(valueBytes));
+
+            response.data = new TeacherRepository().countTeacher(stringFilter);
+            response.oparationStatus = Status.OK;
+
+            return StatusCode(200, response);
+        } catch (Exception e) {
+            response.oparationStatus = Status.NOK;
+            response.message = e.ToString();
+            return StatusCode(500, response);
+        }
+    }
+
+    [HttpGet]
+    [Route("countTeachers")]
+    public ActionResult<OperationResponseModel> countTeacher([FromHeader] String userToken) {
+        OperationResponseModel response = new();
+
+        if (!new UserRepository().validateToken(userToken)) {
+            response.oparationStatus = Status.NOK;
+            response.message = "userToken Inválido.";
+            return StatusCode(401, response);
+        }
+        long result = new TeacherRepository().countTeacher();
+        response.oparationStatus = Status.OK;
+        response.data = result;
+        return Ok(response);
+    }
+
+    [HttpGet]
+    [Route("getTeacherById")]
+    public ActionResult<OperationResponseModel> getTeacherById([FromHeader] String userToken, [FromQuery] string codigo) {
+        OperationResponseModel response = new();
+
+        if (!new UserRepository().validateToken(userToken)) {
+            response.oparationStatus = Status.NOK;
+            response.message = "userToken Inválido.";
+            return StatusCode(401, response);
+        }
+
+        TeacherModel result = new TeacherRepository().getTeacherById(codigo);
+        response.oparationStatus = Status.OK;
+        response.data = result;
+
+        return StatusCode(200, response);
+    }
+
+
+}
---
+++ Repository/Implementations/TeacherRepository.cs
@@ -0,0 +1,122 @@
+﻿using MongoDB.Driver;
+using Newtonsoft.Json;
+using SingularChatAPIs.BD;
+using SingularChatAPIs.Models;
+using SingularChatAPIs.Repository.Interfaces;
+using System.Reflection;
+
+namespace SingularChatAPIs.Repository.Implementations;
+public class TeacherRepository : ITeacherRepository {
+
+    private readonly IMongoCollection<TeacherModel> collection;
+
+    public TeacherRepository() {
+        IMongoDatabase database = MongoDBConnection.getMongoDatabase();
+        collection = database.GetCollection<TeacherModel>("Teacher");
+    }
+
+    public bool addTeacher(TeacherModel teacher, UserModel user) {
+        try {
+            teacher.dataController = new() {
+                active = true,
+                userPost = user.userToken,
+                postDate = DateTime.UtcNow.AddHours(-3)
+            };
+
+            collection.InsertOne(teacher);
+            return true;
+        } catch (Exception) {
+            return false;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    public long countTeacher(string query) {
+        dynamic? queryObject = JsonConvert.DeserializeObject<dynamic>(query);
+        if (queryObject == null) {
+            return 0;
+        }
+        queryObject["dataController.active"] = true;
+        string finalQueryString = JsonConvert.SerializeObject(queryObject);
+
+        return collection.Find(finalQueryString).CountDocuments();
+    }
+
+    public long countTeacher() {
+        return collection.CountDocuments<TeacherModel>(DOC => DOC.dataController.active);
+    }
+
+    public bool deleteTeacher(string id) {
+        try {
+            DeleteResult result = collection.DeleteOne<TeacherModel>(DOC => DOC.codigo == id);
+            return true;
+        } catch (Exception) {
+            return false;
+        }
+    }
+
+    public TeacherModel getTeacherById(string id) {
+        try {
+            TeacherModel personModelResult = collection.Find<TeacherModel>(DOC => DOC.codigo.Equals(id) && DOC.dataController.active == true).FirstOrDefault();
+            return personModelResult;
+        } catch (Exception) {
+            return null;
+        }
+    }
+
+    public List<TeacherModel> getTeacherByStringQuery(string query, int skip, int take) {
+        try {
+            dynamic? queryObject = JsonConvert.DeserializeObject<dynamic>(query);
+            if (queryObject == null) {
+                throw new Exception();
+            }
+            queryObject["dataController.active"] = true;
+
+            string finalQueryString = JsonConvert.SerializeObject(queryObject);
+
+            return collection.Find<TeacherModel>(finalQueryString).Skip(skip).Limit(take).ToList<TeacherModel>();
+        } catch (Exception) {
+            return new List<TeacherModel>();
+        }
+    }
+
+    public List<TeacherModel> getTeacherList(int skip, int take) {
+        try {
+            List<TeacherModel> result = collection.Find<TeacherModel>(DOC => DOC.dataController.active == true).Skip(skip).Limit(take).ToList<TeacherModel>();
+            return result;
+        } catch (Exception) {
+            return null;
+        }
+    }
+
+    public bool logicalDeleteTeacher(string id, UserModel user) {
+        try {
+            List<UpdateDefinition<TeacherModel>> updateDefinitions = new() {
+                Builders<TeacherModel>.Update.Set(DOC => DOC.dataController.updateDate, DateTime.UtcNow.AddHours(-3)),
+                Builders<TeacherModel>.Update.Set(DOC => DOC.dataController.userUpdate, user.userToken),
+                Builders<TeacherModel>.Update.Set(DOC => DOC.dataController.active, false)
+            };
+            UpdateResult result = collection.UpdateOne<TeacherModel>(DOC => DOC.codigo.Equals(id), Builders<TeacherModel>.Update.Combine(updateDefinitions));
+            return result.ModifiedCount > 0;
+        } catch (Exception) {
+            return false;
+        }
+    }
+
+    public bool updateTeacher(TeacherModel teacher, UserModel user) {
+        TeacherModel currentTeacher = collection.Find<TeacherModel>(DOC => DOC.codigo.Equals(teacher.codigo)).FirstOrDefault();
+        if (currentTeacher == null) { return false; }
+
+        List<UpdateDefinition<TeacherModel>> updateDefinitions = new();
+        foreach (PropertyInfo property in teacher.GetType().GetProperties()) {
+            updateDefinitions.Add(Builders<TeacherModel>.Update.Set(property.Name, property.GetValue(teacher)));
+        }
+        currentTeacher.dataController.updateDate = DateTime.UtcNow.AddHours(-3);
+        currentTeacher.dataController.userUpdate = user.userToken;
+        updateDefinitions.Add(Builders<TeacherModel>.Update.Set("dataController", currentTeacher.dataController));
+
+        UpdateResult result = collection.UpdateOne<TeacherModel>(DOC => DOC.codigo.Equals(teacher.codigo), Builders<TeacherModel>.Update.Combine(updateDefinitions));
+        return result.ModifiedCount > 0;
+    }
+}
