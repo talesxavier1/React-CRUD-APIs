d7ab3caf3b41128be460a61b7b22cc1e43b53f0e
Tales Xavier <talesxavier1@hotmail.com>
2023-06-14 22:51:16 -0300

    Criação. Criação do modelo SpokenLanguage.

---
+++ Models/SpokenLanguagesModel.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace SingularChatAPIs.Models;
+public class SpokenLanguagesModel {
+
+    [BsonId]
+    [BsonElement("codigo")]
+    public string id { get; set; }
+
+    [BsonElement("codigoRef")]
+    public string codigoRef { get; set; }
+
+    [BsonElement("nomeIdioma")]
+    public string languageName { get; set; }
+
+    [BsonElement("nivelProeficiencia")]
+    public string proficiencyLevel { get; set; }
+
+    [BsonElement("aplicacoesPraticas")]
+    public string practicalApplications { get; set; }
+
+    [BsonElement("dataController")]
+    public ControllerModel dataController;
+
+    public SpokenLanguagesModel() {
+        this.id = Guid.NewGuid().ToString();
+    }
+
+}
+
