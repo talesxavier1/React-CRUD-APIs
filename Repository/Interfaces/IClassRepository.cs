081db1ce51c9f85675b3c2b53df19a5eb582dd7c
Tales Xavier <talesxavier1@hotmail.com>
2023-07-14 22:50:41 -0300

    Criação. Criação da interface IClassRepository.

---
+++ Repository/Interfaces/IClassRepository.cs
@@ -0,0 +1,14 @@
+﻿using SingularChatAPIs.Models;
+
+namespace SingularChatAPIs.Repository.Interfaces;
+public interface IClassRepository {
+    public Boolean addClass(ClassModel classModel, UserModel user);
+    public ClassModel getClassById(String id);
+    public List<ClassModel> getClassByStringQuery(string query, int skip, int take);
+    public List<ClassModel> getClassesList(int skip, int take);
+    public long countClassesByQuery(string query);
+    public long countClasses();
+    public Boolean updateClass(ClassModel classModel, UserModel user);
+    public Boolean deleteClass(string id);
+    public Boolean logicalDeleteClass(string id, UserModel user);
+}
