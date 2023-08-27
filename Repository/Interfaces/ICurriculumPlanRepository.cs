26cca9f89268276c5f1d04c7cb313bffbbfb338c
Tales Xavier <talesxavier1@hotmail.com>
2023-08-27 20:03:31 -0300

    Ajuste. Adição do campo codigoRef;

--- Repository/Interfaces/ICurriculumPlanRepository.cs
+++ Repository/Interfaces/ICurriculumPlanRepository.cs
@@ -3,12 +3,12 @@
 namespace SingularChatAPIs.Repository.Interfaces;
 public interface ICurriculumPlanRepository {
     public Boolean AddCurriculumPlan(CurriculumPlanModel curriculumPlanModel, UserModel user);
-    public List<CurriculumPlanModel> getCurriculumPlans(int skip, int take);
-    public List<CurriculumPlanModel> getCurriculumPlans(int skip, int take, String query);
+    public List<CurriculumPlanModel> getCurriculumPlans(int skip, int take, string? codigoRef);
+    public List<CurriculumPlanModel> getCurriculumPlans(int skip, int take, String query, string? codigoRef);
     public CurriculumPlanModel getCurriculumPlanById(string codigo);
     public Boolean updateCurriculumPlan(CurriculumPlanModel curriculumPlanModel, UserModel User);
     public Boolean deleteCurriculumPlan(String codigo);
     public Boolean logicalDeleteCurriculumPlan(String[] codigos, UserModel user);
-    public long count();
-    public long count(String query);
+    public long count(string? codigoRef);
+    public long count(String query, string? codigoRef);
 }
